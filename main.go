b001f64931628427f10e76447afd9ab99a06e937
fess932 <fess932@gmail.com>
2020-06-02 13:32:55 +0300

    asdf

--- main.go
+++ main.go
@@ -10,10 +10,8 @@ type HttpHandler struct{}
 // implement `ServeHTTP` method on `HttpHandler` struct
 func (h HttpHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 
-	// create response binary data
 	data := []byte("Hello World!") // slice of bytes
 
-	// write `data` to response
 	res.Write(data)
 }
 
@@ -23,6 +21,6 @@ func main() {
 	handler := HttpHandler{}
 
 	// listen and serve
-	http.ListenAndServe(":9000", handler)
+	http.ListenAndServe(":80", handler)
 
 }
